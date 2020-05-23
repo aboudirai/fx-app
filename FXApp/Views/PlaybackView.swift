44f4f55cfcef740a377c0202c130c38d24799cc2
aboudirai <raiaboudi@yahoo.com>
2020-05-23 10:47:46 -0400

    pause tap gesture

--- FXApp/Views/PlaybackView.swift
+++ FXApp/Views/PlaybackView.swift
@@ -11,24 +11,40 @@ import AVFoundation
 
 class PlaybackView: UIView {
     var looper: AVPlayerLooper?
+    var player: AVQueuePlayer?
+    var isPlaying: Bool = true
     
     init(frame: CGRect, item: AVPlayerItem){
         super.init(frame: frame)
         
         backgroundColor = .black
         
-        let player = AVQueuePlayer(playerItem: item)
+        //Adding Pause/Play Tap Gesture
+        let pauseGesture = UITapGestureRecognizer(target: self, action: #selector(self.pauseGestureHandler(sender:)))
+        addGestureRecognizer(pauseGesture)
+        
+        //Playing Video on Loop
+        player = AVQueuePlayer(playerItem: item)
         let playerLayer: AVPlayerLayer = AVPlayerLayer(player: player)
         //playerLayer.cornerRadius = 20
         self.layer.addSublayer(playerLayer)
         playerLayer.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        looper = AVPlayerLooper(player: player, templateItem: item)
+        looper = AVPlayerLooper(player: player!, templateItem: item)
         
-        player.play()
+        player!.play()
     }
-     
+    
+    @objc func pauseGestureHandler(sender: UITapGestureRecognizer){
+        if isPlaying {
+            player!.pause()
+        }
+        else{
+            player!.play()
+        }
+        isPlaying = !isPlaying
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
